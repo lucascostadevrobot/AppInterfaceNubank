68a8c6185079de9b419eae551689ab4626bfbc1d
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2022-06-28 17:14:38 -0300

    Commit inicial app
    Enviando primeiro commit com a versão Demo do aplicativo.

---
+++ app/src/main/java/lucascosta/dev/appcustomizandolayout/MainActivity.java
@@ -0,0 +1,21 @@
+package lucascosta.dev.appcustomizandolayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
